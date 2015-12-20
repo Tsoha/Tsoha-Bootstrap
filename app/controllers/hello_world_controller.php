253533dc9b1923c8db50fcfcc7e6eda58110c0d9
Jarkko Lehtiranta <jarkkolehtiranta@Jarsba-Mac-Mini.local>
2015-12-21 01:14:04 +0200

    Käyttöliittymiä suunniteltu, sivuja lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,34 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
+     public static function memorylist(){
+    View::make('suunnitelmat/memorylist.html');
   }
+
+  public static function login(){
+    View::make('suunnitelmat/login.html');
+  }
+
+  public static function edit_memory(){
+    View::make('suunnitelmat/edit_memory.html');
+  }
+  public static function memo_page(){
+    View::make('suunnitelmat/memo_page.html');
+  }
+
+  public static function reg(){
+    View::make('suunnitelmat/reg.html');
+  }
+
+
+}
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,21 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/login', function() {
+  HelloWorldController::login();
+});
+$routes->get('/memorylist', function() {
+  HelloWorldController::memorylist();
+});
+
+$routes->get('/reg', function() {
+  HelloWorldController::reg();
+});
+
+$routes->get('/edit_memory', function() {
+  HelloWorldController::edit_memory();
+});
+$routes->get('/memo_page', function() {
+  HelloWorldController::memo_page();
+});
