59a2a83529d68ec519126632db61645178aa8324
Jarkko Lehtiranta <jarkkolehtiranta@Jarsba-Mac-Mini.local>
2015-12-31 19:59:36 +0200

    Routes ja Controllerit päivitetty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -11,24 +11,25 @@ class HelloWorldController extends BaseController {
         // Testaa koodiasi täällä
         View::make('helloworld.html');
     }
-     public static function memorylist(){
-    View::make('suunnitelmat/memorylist.html');
-  }
 
-  public static function login(){
-    View::make('suunnitelmat/login.html');
-  }
+    public static function memorylist() {
+        View::make('memorylist.html');
+    }
 
-  public static function edit_memory(){
-    View::make('suunnitelmat/edit_memory.html');
-  }
-  public static function memo_page(){
-    View::make('suunnitelmat/memo_page.html');
-  }
+    public static function login() {
+        View::make('login.html');
+    }
 
-  public static function reg(){
-    View::make('suunnitelmat/reg.html');
-  }
+    public static function edit_memory() {
+        View::make('edit_memory.html');
+    }
 
+    public static function memo_page() {
+        View::make('memo_page.html');
+    }
+
+    public static function reg() {
+        View::make('reg.html');
+    }
 
 }
--- config/routes.php
+++ config/routes.php
@@ -1,27 +1,27 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
-  
-  $routes->get('suunnitelmat/login', function() {
-  HelloWorldController::login();
-});
-$routes->get('suunnitelmat/memorylist', function() {
-  HelloWorldController::memorylist();
 });
 
-$routes->get('suunnitelmat/reg', function() {
-  HelloWorldController::reg();
+$routes->get('/login.html', function() {
+    HelloWorldController::login();
+});
+$routes->get('/memorylist.html', function() {
+    HelloWorldController::memorylist();
 });
 
-$routes->get('suunnitelmat/edit_memory', function() {
-  HelloWorldController::edit_memory();
+$routes->get('/reg.html', function() {
+    HelloWorldController::reg();
 });
-$routes->get('suunnitelmat/memo_page', function() {
-  HelloWorldController::memo_page();
+
+$routes->get('/edit_memory.html', function() {
+    HelloWorldController::edit_memory();
+});
+$routes->get('/memo_page.html', function() {
+    HelloWorldController::memo_page();
 });
